a27f69b82546c5b6fde0f1770bfba1bf6a5017f2
榊原昌彦 <sakakibara@rdlabo.jp>
2016-09-18 12:09:56 +0900

    templateを条件に揃える

--- ionic2-beta11/app/pages/home/home.ts
+++ ionic2-beta11/app/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  template: 'Hello World!'
 })
 export class HomePage {
   constructor(public navCtrl: NavController) {
