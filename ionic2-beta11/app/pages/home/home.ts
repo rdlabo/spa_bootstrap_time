c86acac570e347a0c2f6d45874677b92bd138f5f
榊原昌彦 <sakakibara@rdlabo.jp>
2016-09-18 12:11:45 +0900

    navcontrollerの読み込みをキャンセル

--- ionic2-beta11/app/pages/home/home.ts
+++ ionic2-beta11/app/pages/home/home.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   template: 'Hello World!'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
   }
 }
